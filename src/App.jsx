b977d9d9566494c9c066aa3c55dd4317291d2925
PAWANPATEL <pp467354@gmail.com>
2024-09-19 15:52:27 +0530

    added my project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+import Login from './Login'
+import Signup from './Signup'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='parent'>
+    <Login/>
+    <Signup/></div>
+      
+    </>
+  )
+}
+
+export default App
